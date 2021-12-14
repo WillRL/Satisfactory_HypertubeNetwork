7cc55321375956e8b984da48d6f2ba05a88bdfa4
William Li <wlii0044@student.monash.edu>
2021-12-14 16:43:24 +1100

    Fixed error led by incorrect concatenation of string

--- lib/MainComputer.lua
+++ lib/MainComputer.lua
@@ -61,7 +61,7 @@ function run(size, debug)
 
 
         elseif mode == "generate_path" then
-            print("Generating Path: "..data1 "to"..hyper_network_dest_vertices[current_entrance])
+            print("Generating Path: "..data1.."to"..hyper_network_dest_vertices[current_entrance])
 
             local path = hyper_network:generate_path(data1,hyper_network_dest_vertices[current_entrance])
             local path_string
