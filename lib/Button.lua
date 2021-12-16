9f83da50243911c62ac7d711fc65ad3011c34e71
William Li <wlii0044@student.monash.edu>
2021-12-17 00:25:01 +1100

    Added methods to change background and foreground for buttons

--- lib/Button.lua
+++ lib/Button.lua
@@ -4,7 +4,7 @@
 
 
 
-local UPDATED = "17/12/2021 12:21am"
+local UPDATED = "17/12/2021 12:24am"
 print("Initialising Button.lua\nLast Update: "..UPDATED)
 
 filesystem.doFile("Boundary.lua")
@@ -40,6 +40,15 @@ function Button:draw(gpu)
     local label_start = midX-label_len+label_mid
 
     gpu:setText(label_start, midY, self.label)
+end
+
+function Button:setBackground(rgba)
+    gpu:setBackground(rgba[1], rgba[2], rgba[3], rgba[4])
+end
+
+
+function Button:setForeground(rgba)
+    gpu:setForeground(rgba[1], rgba[2], rgba[3], rgba[4])
 end
 
 
