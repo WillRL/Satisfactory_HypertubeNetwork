b58d7d5971a364ddffa1c2be37ea0012ad764d21
William Li <wlii0044@student.monash.edu>
2021-12-17 20:17:03 +1100

    Added in screen support in Auxiliary Computer's

--- AuxiliaryComputer_InGame.lua
+++ AuxiliaryComputer_InGame.lua
@@ -9,6 +9,10 @@ local VERTEX = nil
 local CONNECTIONS = {}
 local NAME = nil
 
+local gpu = computer.getPCIDevices(findClass("GPUT1"))[1]
+local screen = component.proxy(component.findComponent(findClass("Screen"))[1])
+
+
 fs = filesystem
 
 if fs.initFileSystem("/dev") == false then
@@ -27,7 +31,36 @@ local _, libdata = req:await()
 local file = fs.open("AuxiliaryComputer.lua", "w")
 file:write(libdata)
 file:close()
-
 fs.doFile("AuxiliaryComputer.lua")
 
-run(VERTEX, CONNECTIONS, NAME)
+if screen and NAME then
+    gpu:bindScreen(screen)
+    event.listen(gpu)
+
+    local req1 = card:request("https://raw.githubusercontent.com/WillaLR/Satisfactory_HypertubeNetwork/master/lib/Button.lua", "GET", "")
+    local req2 = card:request("https://raw.githubusercontent.com/WillaLR/Satisfactory_HypertubeNetwork/master/lib/PageScroller.lua", "GET", "")
+    local req3 = card:request("https://raw.githubusercontent.com/WillaLR/Satisfactory_HypertubeNetwork/master/lib/Boundary.lua", "GET", "")
+
+    local _, libdata1 = req1:await()
+    local _, libdata2 = req2:await()
+    local _, libdata3 = req3:await()
+
+    local file1 = fs.open("Button.lua", "w")
+    file1:write(libdata1)
+    file1:close()
+
+    local file2 = fs.open("PageScroller.lua", "w")
+    file2:write(libdata2)
+    file2:close()
+
+    local file3 = fs.open("Boundary.lua", "w")
+    file3:write(libdata3)
+    file3:close()
+
+    fs.doFile("PageScroller.lua")
+    fs.doFile("AuxiliaryComputer.lua")
+
+    run_with_screen(VERTEX, CONNECTIONS, NAME, screen, gpu)
+else
+    run(VERTEX, CONNECTIONS, NAME)
+end
--- lib/AuxiliaryComputer.lua
+++ lib/AuxiliaryComputer.lua
@@ -3,7 +3,7 @@
 --- DateTime: 15/12/2021 11:35 pm
 ---
 
-local UPDATED = "16/12/2021 12:39am"
+local UPDATED = "17/12/2021 8:16am"
 print("Initialising AuxiliaryComputer.lua\nLast Update:"..UPDATED)
 
 local function init(vertex, connections, name, NetworkCard)
@@ -45,6 +45,162 @@ local function extract_edges(path, vertex)
     end
 end
 
+function run_with_screen(vertex, connections, vertex_name, screen, gpu)
+    local NetworkCard = computer.getPCIDevices(findClass("NetworkCard"))[1]
+
+    local w, h = screen:getSize()
+    local scale = 1.25
+
+    local panel_scale = 0.7
+
+    gpu:setSize(math.floor(scale * w * 32), math.floor(scale * h * 15))
+    w, h = gpu:getSize()
+    print(w, h)
+
+    local panel_width = math.floor(w * panel_scale)
+    local panel_x_mid = math.floor((panel_width) / 2) + 2
+
+    local up_button = Button("  ↑", panel_x_mid - w / 10, panel_x_mid + w / 10 + 1, 3, 4, { 0, 0.2, 0, 1 }, { 1, 1, 1, 1 })
+    local down_button = Button("  ↓", panel_x_mid - w / 10, panel_x_mid + w / 10 + 1, h - 4, h - 3, { 0, 0.2, 0, 1 }, { 1, 1, 1, 1 })
+
+    local current_dest_label = Button("Current Location", panel_width + 2, w - 3, 3, 4, { 0.05, 0.05, 0.5, 1 }, { 1, 1, 1, 1 })
+    local current_dest_text = Button(NAME, panel_width + 2, w - 3, 4, 5, { 0, 0, 0, 1 }, { 1, 1, 1, 1 })
+
+    local routing_info_label = Button("Route Info", panel_width + 2, w - 3, 6, 7, { 0.05, 0.05, 0.5, 1 }, { 1, 1, 1, 1 })
+    local routing_from_label = Button("From", panel_width + 2, w - 3, 7, 8, { 0, 0, 0, 1 }, { 1, 0.6, 0.2, 1 })
+    local routing_to_label = Button("To", panel_width + 2, w - 3, 9, 10, { 0, 0, 0, 1 }, { 1, 0.6, 0.2, 1 })
+
+    local routing_from_text = Button(NAME, panel_width + 2, w - 3, 8, 9, { 0, 0, 0, 1 }, { 1, 1, 1, 1 })
+    local routing_to_text = Button("", panel_width + 2, w - 3, 10, 11, { 0, 0, 0, 1 }, { 1, 1, 1, 1 })
+
+    local route_button = Button("Compute Route", panel_width + 2, w - 3, 12, 15, { 0, 0.25, 0, 1 }, { 1, 1, 1, 1 })
+
+    local page = PageScroller(5, panel_width + 1, 4, h - 4)
+
+    local function refresh()
+        gpu:setBackground(0, 0, 0, 1)
+        gpu:fill(0, 0, w, h, " ")
+
+        gpu:setBackground(0.1, 0.1, 0.1, 1)
+        gpu:fill(2, 1, w - 4, h - 2, " ")
+
+        gpu:setBackground(0, 0, 0, 1)
+        gpu:fill(2 + 2, 1 + 1, panel_width - 4, h - 4, " ")
+
+        gpu:setBackground(0, 4, 0, 1)
+
+        up_button:draw(gpu)
+        down_button:draw(gpu)
+        current_dest_label:draw(gpu)
+        current_dest_text:draw(gpu)
+
+        routing_info_label:draw(gpu)
+        routing_from_label:draw(gpu)
+        routing_to_label:draw(gpu)
+
+        routing_from_text:draw(gpu)
+        routing_to_text:draw(gpu)
+
+        route_button:draw(gpu)
+
+        page:draw(gpu)
+    end
+
+    local function scroll_up(_, bool)
+        if bool then
+            page:scroll("vertical", 0, 2)
+        end
+
+    end
+
+    local function scroll_down(_, bool)
+        if bool then
+            page:scroll("vertical", 0, -2)
+        end
+    end
+
+    local function hover(button, bool)
+        if bool then
+            button:setForeground({ 0, 1, 0, 1 })
+        else
+            button:setForeground({ 1, 1, 1, 1 })
+        end
+    end
+
+    local function select(button, bool)
+        if bool then
+            button:setBackground({ 0, 0.1, 0, 1 })
+            routing_to_text:set_label(button:get_label())
+        else
+            button:setBackground({ 0, 0, 0, 1 })
+        end
+    end
+
+    local function route(_, bool)
+        if bool then
+            NetworkCard:broadcast(00000, "generate_path", vertex, routing_to_text:get_label())
+        end
+    end
+
+    local button_back = { 0, 0, 0, 1 }
+    local button_fore = { 1, 1, 1, 1 }
+    local OFFSET = 1
+
+    page:draw(gpu)
+
+    refresh()
+    gpu:flush()
+    page:add_button("New Destination", 5, panel_width - 1, 4, 5, button_back, { 1, 1, 1, 1 })
+    while true do
+        local e, _, x, y, mode, data = event.pull()
+        if e == "OnMouseDown" then
+            print(x, y)
+            up_button:execute(x, y, scroll_up)
+            down_button:execute(x, y, scroll_down)
+            page:execute(x, y, select, false)
+            route_button:execute(x, y, route)
+            --gpu:setText(x, y, " ")
+        elseif e == "OnMouseMove" then
+            page:execute(x, y, hover)
+
+
+        elseif mode == "new_path" then
+            local prev, after = extract_edges(data, vertex)
+            local switches, switch
+            local switched = {}
+
+            for i=1, #connections do
+                switches = component.findComponent(tostring(connections[i]))
+                print(connections[i], prev, after)
+                for j=1, #switches do
+                    switch = component.proxy(switches[j])
+                    if connections[i] == tonumber(prev) or connections[i] == tonumber(after) then
+                        switch.isSwitchOn = true
+                        switched[switches[j]] = true
+
+                    elseif switched[switches[j]] == nil then
+                        switch.isSwitchOn = false
+                    end
+                end
+            end
+
+        elseif mode == "auxiliary" and vertex_name ~= nil then
+            if page:button_count() == 0 then
+                page:add_button(data, 5, panel_width - 1, 4, 5, button_back, { 1, 1, 1, 1 })
+            else
+                page:add_button_sequential(data, 0, OFFSET, button_back, button_fore)
+            end
+            page:draw(gpu)
+            print("Receiving data for new destinations")
+
+        elseif mode == "update_software" then
+            computer.reset()
+            print("Updating Software")
+        end
+        refresh()
+        gpu:flush()
+    end
+end
 
 function run(vertex, connections, vertex_name)
     --- Initial function to initialise.
